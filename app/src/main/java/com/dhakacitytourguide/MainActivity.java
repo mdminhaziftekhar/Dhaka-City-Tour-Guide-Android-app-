e9898f41217a96cd65df4a9d2e581b31fe6215b1
mdminhaziftekhar <mdminhaziftekhar@gmail.com>
2022-01-29 13:10:07 +0600

    initial

---
+++ app/src/main/java/com/dhakacitytourguide/MainActivity.java
@@ -0,0 +1,28 @@
+package com.dhakacitytourguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Find the viewpager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        //Create a pager adapter
+
+        //Set the adapter onto the view pager
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/dhakacitytourguide/fragmentPagerAdapter.java
@@ -0,0 +1,55 @@
+package com.dhakacitytourguide;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class fragmentPagerAdapter extends FragmentPagerAdapter {
+
+    private static final int PAGE_COUNT = 4;
+    private Context context;
+
+    public fragmentPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        if(position == 0){
+            return new placesFragment();
+        } else if(position == 1){
+            return new restaurantsFragment();
+        } else if(position == 2) {
+            return new mosquesFragment();
+        } else {
+            return new hospitalsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0 :
+                return "Places";
+            case 1 :
+                return "Restaurants";
+            case 2 :
+                return "Mosques";
+            case 3 :
+                return "Hospitals";
+            default : return null;
+        }
+    }
+}
